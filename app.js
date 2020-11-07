46b22174ccbe83e79acd4c006a5703d420e01369
wiyaou mangazie <mariejoseph144@gmail.com>
2020-11-07 05:01:47 -0500

    second commit

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.post("/", function(req, res){
     }
 
     const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200 ){
+            res.sendFile(__dirname + "/success.html")
+        }else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -51,6 +58,13 @@ app.post("/", function(req, res){
         
 });
 
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+app.post("/success", function(req, res){
+    res.redirect("/")
+})
 
 
 app.listen(3080, function(){
