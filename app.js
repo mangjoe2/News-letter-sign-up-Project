414898af379c3a78a39cd60ef5abe03faa5df499
wiyaou mangazie <mariejoseph144@gmail.com>
2020-11-07 02:35:11 -0500

    Third commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { post } = require("request");
 
 const app = express();
 
@@ -9,16 +11,47 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
-})
+});
 
 app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(email, firstName, lastName);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }   
+        ]
+    };
 
-})
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us2.api.mailchimp.com/3.0/lists/502a85eb5d"
+
+    const options = {
+        method: "POST",
+        auth: "joseph2:fadaeee0dc51b0221581951b6f064a3b-us2"
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+        
+});
+
+
 
 app.listen(3080, function(){
     console.log("The server is running on port 3080");
